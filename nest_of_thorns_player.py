34a7162a70a605dfeba2b3d128a93ec4555b18c2
Reza Jebeli <rezajebeli97@gmail.com>
2025-02-01 22:40:55 -0500

    Move towards treasures, but still buggy picking the options.

--- nest_of_thorns_player.py
+++ nest_of_thorns_player.py
@@ -25,34 +25,48 @@ class NestOfThornsAI:
         self.attempt_number += 1
 
         # Detect and click each required button based on text
-        self.wait_and_click_text(800,950, 'PLAY')
-        self.wait_and_click_text(500,600, 'DRAGONUS')
-        self.wait_and_click_text(800,950, 'NEXT')
-        self.wait_and_click_text(800,950, 'START')
+        self.wait_and_click_text(800,950, 'PLAY', 0.3)
+        self.wait_and_click_text(500,600, 'DRAGONUS', 0.1)
+        self.wait_and_click_text(800,950, 'NEXT', 0.3)
+        self.wait_and_click_text(800,950, 'START', 0.0)
 
         self.wait_for_the_game_to_start()
         print('Game has started!')
 
-        directions = self.detect_treasures_and_direction()
+        directions, treasure_positions = self.detect_treasures_and_direction()
 
-        if directions[0] == directions[1]:
-            print(f"Treasures are in the same direction: {directions[0]}")
+        if directions:
+            if directions[0] == directions[1]:
+                print(f"Treasures are in the same direction: {directions[0]}")
+            else:
+                print(f"Treasures are not in the same direction: {directions}")
+                self.restart()
         else:
-            print(f"Treasures are not in the same direction: {directions}")
             self.restart()
 
+        first_treasure, second_treasure = self.sort_treasure_based_on_distance(treasure_positions)
+
+        self.move_towards_treasure(first_treasure)
+
+        self.move_towards_treasure(second_treasure)
+
+        self.pause_the_game()
 
 
+
+    def pause_the_game(self):
+        print('Your lucky start. Good luck!')
+        pyautogui.press('esc')
+        sys.exit()
 
     def restart(self):
         print('Unlucky: Restarting the game!')
         pyautogui.press('esc')
-        time.sleep(0.5)
-        self.wait_and_click_text(852, 631, 'Leave Game')
-        time.sleep(0.5)
+        time.sleep(0.3)
+        self.wait_and_click_text(852, 631, 'Leave Game', 1)
         self.start_game()
 
-    def wait_and_click_text(self, x, y, target_text):
+    def wait_and_click_text(self, x, y, target_text, wait_time):
         """
         Continuously captures the screen and waits for the specified text to appear, then clicks it.
         
@@ -60,7 +74,7 @@ class NestOfThornsAI:
         """
         pyautogui.click(x, y)
         print(f"Clicked on '{target_text}' at ({x}, {y}).")
-        time.sleep(0.5)  # Give time for the game to load the next screen
+        time.sleep(wait_time)  # Give time for the game to load the next screen
 
     def wait_for_the_game_to_start(self):
         while True:
@@ -104,14 +118,14 @@ class NestOfThornsAI:
 
         if len(treasure_positions) != 2:
             print("Could not detect exactly two treasures.")
-            return False
+            return False, False
 
         # Determine if treasures are in the same direction
         directions = self.evaluate_treasure_direction(treasure_positions)
 
         self.save_minimap(treasure_positions, minimap, directions)
 
-        return directions
+        return directions, treasure_positions
 
 
     def detect_treasures_with_color_filtering(self, minimap):
@@ -238,6 +252,120 @@ class NestOfThornsAI:
         while self.running:
             # Add further logic for in-game interactions here
             pass
+
+    def sort_treasure_based_on_distance(self, treasure_positions):
+        minimap_center = (260, 260)  # Center of the minimap after resizing to 520x520
+
+        # Calculate distance of each treasure from the center
+        distances = []
+        for (x, y) in treasure_positions:
+            distance = np.sqrt((x - minimap_center[0])**2 + (y - minimap_center[1])**2)
+            distances.append(distance)
+
+        # Find the index of the closer treasure
+        closest_index = np.argmin(distances)
+        closest_treasure = treasure_positions[closest_index]
+        return treasure_positions[closest_index], treasure_positions[closest_index-1]
+
+
+    def move_towards_treasure(self, treasure_position):
+        """
+        Moves the player towards the closer treasure based on distance from the minimap center.
+
+        :param treasure_positions: List of (x, y) coordinates of the detected treasures.
+        """
+        screen_center = (863, 569)
+        minimap_center = (260, 260)  # Center of the minimap after resizing to 520x520
+
+        while(not self.skill_or_item_selection()):
+            print(f"Moving towards the treasure at {treasure_position}")
+
+            # Calculate the screen coordinates to click
+            # The minimap is at (15, 770) on the screen, and the minimap itself is 520x520
+            click_position_x = screen_center[0] + ((treasure_position[0]- minimap_center[0])*2)
+            click_position_y = screen_center[1] + ((treasure_position[1]- minimap_center[1])*2)
+
+            pyautogui.moveTo(click_position_x, click_position_y)
+            pyautogui.mouseDown()
+            print(f"Clicked at screen coordinates ({click_position_x}, {click_position_y}) to move towards the treasure.")
+
+
+    def skill_or_item_selection(self):
+        """
+        Detects if the skill or item selection screen is open using OCR.
+        Looks for the words 'ACQUIRE' or 'UPGRADE' on the screen.
+        """
+        # Capture the region where buttons like 'ACQUIRE' or 'UPGRADE' are located
+        # Adjust the region based on your screen resolution and game UI layout
+
+        if self.has_text((812, 793, 100, 40), 'acquire'):             
+            print("Skill selection screen detected!")
+            if self.has_text((470,380, 200, 40), 'missile'):
+                self.click(574,818, 0.1)
+            elif self.has_text((765,380, 200, 40), 'missile'):
+                self.click(870,818, 0.1)
+            if self.has_text((1050,380, 200, 40), 'missile'):
+                self.click(1150,818, 0.1)
+            elif self.has_text((470,380, 200, 40), 'frost'):
+                self.click(574,818, 0.1)
+            if self.has_text((765,380, 200, 40), 'frost'):
+                self.click(870,818, 0.1)
+            elif self.has_text((1050,380, 200, 40), 'frost'):
+                self.click(1150,818, 0.1)
+            else:
+                self.click(1150,818, 0.1)
+                time.sleep(0.3)
+                self.restart()
+
+        if self.has_text((800, 735, 130, 40), 'equip'): 
+            print("Item selection screen detected!")           
+            if self.has_text((470,460, 200, 40), 'array'):
+                self.click(574,755, 0.1)
+            elif self.has_text((765,460, 200, 40), 'array'):
+                self.click(870,755, 0.1)
+            if self.has_text((1050,460, 200, 40), 'array'):
+                self.click(1150,755, 0.1)
+            elif self.has_text((470,460, 200, 40), 'octarine'):
+                self.click(574,755, 0.1)
+            if self.has_text((765,460, 200, 40), 'octarine'):
+                self.click(870,755, 0.1)
+            elif self.has_text((1050,460, 200, 40), 'octarine'):
+                self.click(1150,755, 0.1)
+            else:
+                self.click(1150,755, 0.1)
+                time.sleep(0.3)
+                self.restart()
+
+            return False
+
+        return False
+
+    def click(self, x, y, holding_time):
+        pyautogui.moveTo(x, y)
+        pyautogui.mouseDown()
+        time.sleep(holding_time)
+        pyautogui.mouseUp()
+
+    def has_text(self, region, required_text):
+        screen_capture = pyautogui.screenshot(region=region)  # Example region where the selection appears
+        screen_capture_np = np.array(screen_capture)
+
+        # Convert the screenshot to grayscale for better OCR accuracy
+        gray_image = cv2.cvtColor(screen_capture_np, cv2.COLOR_BGR2GRAY)
+
+        # Use pytesseract to detect text in the screenshot
+        detected_text = pytesseract.image_to_string(gray_image).lower()
+        
+        # Debug: Print detected text
+        print("Detected Text:", detected_text)
+
+        # Check for keywords that indicate the selection screen is open
+        if required_text in detected_text:
+            print(f"{required_text} found!")
+            return True
+
+        return False
+
 
 # Start AI
 ai = NestOfThornsAI()
